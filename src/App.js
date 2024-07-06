0cd55086d74f481803cfd15c5e2ecba96365aeff
Ansh Gupta <guptaansh6031@gmail.com>
2024-07-06 13:14:36 +0530

    video 16 completed but with new syntax of react-router-dom

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import TextForm from './components/TextForm';
 import About from './components/About';
 import Alert from './components/Alert';
 
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
 
 function App() {
 
   const [mode, setMode] = useState("light")
 
-  const toggleMode =()=>{
-    if(mode === "dark"){
+  const toggleMode = () => {
+    if (mode === "dark") {
       (setMode("light"))
       document.body.style.backgroundColor = "white"
       showAlert("Light mode has been Activated.", "success")
     }
-    
-    else{
+
+    else {
       (setMode("dark"))
       document.body.style.backgroundColor = "#313132"
       showAlert("Dark mode has been Activated.", "success")
@@ -28,10 +30,10 @@ function App() {
 
   const [alert, setAlert] = useState(null)
 
-  const showAlert = (message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
-      message : message,
-      type : type,
+      message: message,
+      type: type,
     })
 
 
@@ -44,16 +46,22 @@ function App() {
   return (                                            //  only one element can be returned........
     <>                                      {/* empty jsx fragment, used to wrap all the tags in one tag in order to return...... */}
 
-      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />               {/* bootstrap nav bar imported and used as a component...... */ }       {/* all the javascript will be written in these curly braces... */ }
-                                                                   {/* here, title, aboutText, etc. are props which are sent to the navbar component as variables..... */}
-      
-      <Alert alert={alert} />
-      
-      <div className="container">
-        <TextForm heading="Enter the text to analyze below." mode={mode} showAlert={showAlert} />
-      </div>
+      <Router>
+        
+        <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />               {/* bootstrap nav bar imported and used as a component...... */}       {/* all the javascript will be written in these curly braces... */}
+                                                                                                        {/* here, title, aboutText, etc. are props which are sent to the navbar component as variables..... */}
 
-      <About />
+        <Alert alert={alert} />
+
+        <div className="container">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+
+            <Route exact path="/" element={<TextForm heading="Enter the text to analyze below." mode={mode} showAlert={showAlert} />} />
+          </Routes>
+        </div>
+
+      </Router>
 
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'                      //  write 'impt' for emmet..........
+import { Link } from 'react-router-dom'
 
 
 export default function Navbar(props) {
@@ -11,17 +12,17 @@ export default function Navbar(props) {
 
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
-            <a className="navbar-brand" href="/">{props.title}</a>                  {/* the title sent as a prop is used..... */}
+            <Link className="navbar-brand" to="/">{props.title}</Link>                  {/* the title sent as a prop is used..... */}
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="/">{props.home}</a>                {/* used the default prop. */}
+                        <Link className="nav-link active" aria-current="page" to="/">{props.home}</Link>                {/* used the default prop. */}
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/">{props.aboutText}</a>
+                        <Link className="nav-link" to="/about">{props.aboutText}</Link>
                     </li>
                 </ul>
                 <form className="d-flex mx-4" role="search">
